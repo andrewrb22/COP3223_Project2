086575154e76d91638fd0c67b5c17959245ad5c0
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-06 15:44:28 -0500

    switch statement for number of course

--- Project2.c
+++ Project2.c
@@ -78,6 +78,141 @@ int main()
 
     printf("CRN\tCredit Hours\n");
 
+    // Switch cases based on courses
+
+    switch (numOfCourses)
+
+    {
+
+        // Number of couses = 1
+
+    case 1:
+
+        if (courseID1 == 4587)
+
+        {
+
+            printf("4587\t\t4\t$%.2f", (4 * creditHour));
+
+            totalFee = totalFee + (4 * creditHour);
+        }
+
+        else if (courseID1 == 4599)
+
+        {
+
+            printf("4599\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+
+        else if (courseID1 == 8997)
+
+        {
+
+            printf("8997\t\t1\t$%.2f\n", (1 * creditHour));
+
+            totalFee = totalFee + (1 * creditHour);
+        }
+
+        else if (courseID1 == 9696)
+
+        {
+
+            printf("9696\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+        else{
+printf("\nSorry invalid entry! Try again. Goodbye!\n");
+
+printf("\n-----------------------------\n");
+
+
+
+        }
+
+     break;   
+
+        // Number of courses = 2
+
+    case 2:
+
+        if (courseID1 == 4587)
+
+        {
+
+            printf("4587\t\t4\t$%.2f\n", (4 * creditHour));
+
+            totalFee = totalFee + (4 * creditHour);
+        }
+
+        else if (courseID1 == 4599)
+
+        {
+
+            printf("4599\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+
+        else if (courseID1 == 8997)
+
+        {
+
+            printf("8997\t\t1\t$%.2f\n", (1 * creditHour));
+
+            totalFee = totalFee + (1 * creditHour);
+        }
+
+        else if (courseID1 == 9696)
+
+        {
+
+            printf("9696\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+
+        if (courseID2 == 4587)
+
+        {
+
+            printf("4587\t\t4\t$%.2f\n", (4 * creditHour));
+
+            totalFee = totalFee + (4 * creditHour);
+        }
+
+        else if (courseID2 == 4599)
+
+        {
+
+            printf("4599\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+
+        else if (courseID2 == 8997)
+
+        {
+
+            printf("8997\t\t1\t$%.2f\n", (1 * creditHour));
+
+            totalFee = totalFee + (1 * creditHour);
+        }
+
+        else if (courseID2 == 9696)
+
+        {
+
+            printf("9696\t\t3\t$%.2f\n", (3 * creditHour));
+
+            totalFee = totalFee + (3 * creditHour);
+        }
+
+        break;
+    }
+
     // display the health and id fee
 
     printf("Health & ID Fees \t$%.2f", healthAndIDFee);
